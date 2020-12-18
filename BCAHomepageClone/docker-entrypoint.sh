0d4028b9167da770c94d951f7194ba08908c35d4
Chris <chris@bashbang.com>
2020-12-18 14:45:30 -0800

    changed preview to nginx

--- BCAHomepageClone/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Starting HomeCloneApp App"
-npm run start
-echo $?
-tail -f /dev/null
--- BCAHomepagePreview/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Starting HomeCloneApp App"
-npm start run
-#tail -f /dev/null
